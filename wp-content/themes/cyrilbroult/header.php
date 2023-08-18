616b9f14d7a7b54631c823331aaace8cc4ed91bd
Yannick Liebnau <yannick.liebnau@gmail.com>
2023-08-19 01:25:42 +0200

    [/]FO: header & first homepage view

--- wp-content/themes/cyrilbroult/header.php
+++ wp-content/themes/cyrilbroult/header.php
@@ -28,32 +28,19 @@
 		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'cyrilbroult'); ?></a>
 
 		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if (is_front_page() && is_home()) :
-				?>
-					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-				<?php
-				else :
-				?>
-					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-				<?php
-				endif;
-				$cyrilbroult_description = get_bloginfo('description', 'display');
-				if ($cyrilbroult_description || is_customize_preview()) :
-				?>
-					<p class="site-description"><?php echo $cyrilbroult_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-												?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
-
 			<nav id="site-navigation" class="main-navigation">
+				<?php the_custom_logo(); ?>
 				<?php
 				wp_nav_menu(
 					array(
 						'theme_location' => 'menu-1',
 						'menu_id'        => 'primary-menu',
+					)
+				);
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-2',
+						'menu_id'        => 'secondary-menu',
 					)
 				);
 				?>
