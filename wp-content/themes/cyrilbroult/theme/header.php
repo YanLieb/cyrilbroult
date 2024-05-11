8c3c377fab21781c67f18f030932644647cb516f
Yannick <yannick@MacBook-Pro-de-Yannick.local>
2024-05-11 12:59:05 +0200

    google tag manager & author word on HP

--- wp-content/themes/cyrilbroult/theme/header.php
+++ wp-content/themes/cyrilbroult/theme/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -10,22 +11,45 @@
  * @package cyrilbroult
  */
 
-?><!doctype html>
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-M8BN6RHW');
+    </script>
+    <!-- End Google Tag Manager -->
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class('font-cb-body'); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8BN6RHW" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php wp_body_open(); ?>
 
-<?php wp_body_open(); ?>
+    <div id="page">
+        <a href="#content" class="sr-only"><?php esc_html_e('Skip to content', 'cyrilbroult'); ?></a>
 
-<div id="page">
-	<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'cyrilbroult' ); ?></a>
+        <?php get_template_part('template-parts/layout/header', 'content'); ?>
 
-	<?php get_template_part( 'template-parts/layout/header', 'content' ); ?>
-
-	<div id="content">
+        <div id="content">
